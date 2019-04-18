63679d8fdfd685b62d1223a4b1a3f9aa75777c4c
Himani Garg <garg.himani@outlook.com>
2019-04-18 18:22:00 +0530

    Created HomeScreen

---
+++ lib/Screens/HomeScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+
+          ],
+        ),
+      ),
+    );
+  }
+
+}
