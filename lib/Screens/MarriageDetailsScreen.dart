6311a15860db1c5949586b80a55f2aea2f4f1132
FrazileDevelopers <frazile.com@gmail.com>
2019-05-14 17:46:21 +0530

    MarriageDetailScreen updated

--- lib/Screens/MarriageDetailsScreen.dart
+++ lib/Screens/MarriageDetailsScreen.dart
@@ -1,116 +1,116 @@
 import 'package:flutter/material.dart';
 import '../widget/ProfileCard.dart';
 
-class MarriageDetailsScreen extends StatelessWidget {
+class MarriageDetailsScreen extends StatefulWidget {
+  @override
+  _MarriageDetailsScreenState createState() => _MarriageDetailsScreenState();
+}
+
+class _MarriageDetailsScreenState extends State<MarriageDetailsScreen> {
+  int _selectedPage = 0;
+  final _pageOptions = [Text('Call'), Text('Make a deal')];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          title: Text(
-            "Sahibpreet singh",
-            style: TextStyle(fontSize: 23),
-          ),
-          centerTitle: true,
-          actions: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(right: 10.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(
-                        Icons.location_on,
-                        color: Colors.purple,
-                      ),
-                      Text(
-                        "Location",
-                        style: TextStyle(fontSize: 10),
-                      )
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(right: 10.0),
-                  child: Icon(Icons.menu),
-                ),
-              ],
-            )
-          ],
+      appBar: AppBar(
+        elevation: 0,
+        title: Text(
+          "Sahibpreet singh",
+          style: TextStyle(fontSize: 23),
         ),
-        body: SingleChildScrollView(
-          child: SafeArea(
-            child: Column(
-              children: <Widget>[
-                ProfileCard(),
-                Container(
-                    padding: EdgeInsets.only(top: 20.0),
-                    child: Text(
-                      "Portfolio",
-                      style: TextStyle(
-                          fontSize: 20.0, fontWeight: FontWeight.bold),
-                    )),
-                Container(
-                    height: MediaQuery.of(context).size.height * 0.45,
-                    padding: EdgeInsets.symmetric(
-                        vertical: MediaQuery.of(context).size.height * 0.02),
-                    child: GridView.count(
-                      crossAxisCount: 4,
-                      crossAxisSpacing: 5,
-                      children: List.generate(12, (index) {
-                        return Center(
-                            child: Image(
-                          image: AssetImage('assets/images/bg.png'),
-                        ));
-                      }),
-                    )),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 20.0),
-                  child: Text(
-                    "Show all",
-                    style: TextStyle(color: Colors.blue),
-                  ),
+        centerTitle: true,
+        actions: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(right: 10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      Icons.location_on,
+                      color: Colors.purple,
+                    ),
+                    Text(
+                      "Location",
+                      style: TextStyle(fontSize: 10),
+                    )
+                  ],
                 ),
-                Container(
-                  color: Colors.blue,
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 50.0, vertical: 10.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          Icon(
-                            Icons.phone,
-                            size: 40,
-                            color: Colors.amber,
-                          ),
-                          Text(
-                            "Call",
-                            style: TextStyle(color: Colors.white),
-                          )
-                        ],
-                      ),
-                      Column(
-                        children: <Widget>[
-                          Image(
-                            image: AssetImage("assets/images/deals.png"),
-                            height: 40,
-                          ),
-                          Text(
-                            "Make a deal",
-                            style: TextStyle(color: Colors.white),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                )
-              ],
-            ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 10.0),
+                child: Icon(Icons.menu),
+              ),
+            ],
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Container(
+                  padding: EdgeInsets.only(top: 20.0),
+                  child: Text(
+                    "Portfolio",
+                    style:
+                        TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                  )),
+              Container(
+                  height: MediaQuery.of(context).size.height * 0.45,
+                  padding: EdgeInsets.symmetric(
+                      vertical: MediaQuery.of(context).size.height * 0.02),
+                  child: GridView.count(
+                    crossAxisCount: 4,
+                    crossAxisSpacing: 5,
+                    children: List.generate(12, (index) {
+                      return Center(
+                          child: Image(
+                        image: NetworkImage(
+                            'https://images.pexels.com/photos/1779416/pexels-photo-1779416.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'),
+                      ));
+                    }),
+                  )),
+              Padding(
+                padding: EdgeInsets.only(bottom: 20.0),
+                child: Text(
+                  "Show all",
+                  style: TextStyle(color: Colors.blue),
+                ),
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blue,
+        currentIndex: _selectedPage,
+        onTap: (int index) => setState(() => _selectedPage = index),
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.call,
+                color: Colors.amber,
+              ),
+              title: Text(
+                'Call',
+                style: TextStyle(color: Colors.white),
+              )),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.call,
+              color: Colors.white,
+            ),
+            title: Text(
+              'Make a deal',
+              style: TextStyle(color: Colors.white),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/widget/ProfileCard.dart
+++ lib/widget/ProfileCard.dart
@@ -1,94 +1,137 @@
 import 'package:flutter/material.dart';
 import '../widget/DealsBox.dart';
 
-
-class ProfileCard extends StatelessWidget{
+class ProfileCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Container(
-            width: MediaQuery.of(context).size.width*0.2,
-            child: CircleAvatar(
-              child: Icon(Icons.person),
-              radius: 40,
+    return SafeArea(
+      child: Container(
+        padding: EdgeInsets.all(10.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Container(
+              width: MediaQuery.of(context).size.width * 0.2,
+              child: CircleAvatar(
+                child: Icon(Icons.person),
+                radius: 40,
+              ),
             ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width*0.45,
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding:  EdgeInsets.only(bottom: 10.0),
-                  child: Text("Sahibpreet singh", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-                ),
-                Padding(
-                  padding:  EdgeInsets.all(15.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text("Wedding", style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),),
-                      Text("Events", style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),),
-                      Text("Portfolio", style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold),)
-                    ],
+            Container(
+              width: MediaQuery.of(context).size.width * 0.45,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 10.0),
+                    child: Text(
+                      "Sahibpreet singh",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Column(
-                        children: <Widget>[
-                          Padding(
-                            padding:  EdgeInsets.only(bottom: 10.0),
-                            child: Icon(Icons.favorite_border, size: 40,),
-                          ),
-                          Text('Add to favorite', style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold, color: Colors.blue),)
-                        ],
-                      ),
-                      Column(
-                        children: <Widget>[
-                          Padding(
-                            padding:  EdgeInsets.only(bottom: 10.0),
-                            child: Icon(Icons.share, size: 40,),
-                          ),
-                          Text('Share profile', style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold, color: Colors.blue),)
-                        ],
-                      )
-                    ],
+                  Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          "Wedding",
+                          style: TextStyle(
+                              fontSize: 10, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "Events",
+                          style: TextStyle(
+                              fontSize: 10, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          "Portfolio",
+                          style: TextStyle(
+                              fontSize: 10, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
                   ),
-                )
-              ],
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(bottom: 10.0),
+                              child: Icon(
+                                Icons.favorite_border,
+                                size: 40,
+                              ),
+                            ),
+                            Text(
+                              'Add to favorite',
+                              style: TextStyle(
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blue),
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Padding(
+                              padding: EdgeInsets.only(bottom: 10.0),
+                              child: Icon(
+                                Icons.share,
+                                size: 40,
+                              ),
+                            ),
+                            Text(
+                              'Share profile',
+                              style: TextStyle(
+                                  fontSize: 10,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blue),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width*0.30,
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding:  EdgeInsets.only(bottom: 20.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(Icons.star, color: Colors.amber,),
-                      Icon(Icons.star, color: Colors.amber,),
-                      Icon(Icons.star, color: Colors.amber,),
-                      Icon(Icons.star_border),
-                      Icon(Icons.star_border),
-                    ],
+            Container(
+              width: MediaQuery.of(context).size.width * 0.30,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 20.0),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        Icon(Icons.star_border),
+                        Icon(Icons.star_border),
+                      ],
+                    ),
                   ),
-                ),
-                DealsBox()
-              ],
-            ),
-          )
-        ],
+                  DealsBox()
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
-
 }
